7713d968b10fee29dac740b14c96f435bbfdd2cf
Sasa Gataric <sasagataric@gmail.com>
2021-03-08 13:21:42 +0100

    Added TicketService

---
+++ WinterWorkShop.Cinema.Domain/Services/TicketService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WinterWorkShop.Cinema.Domain.Interfaces;
+
+namespace WinterWorkShop.Cinema.Domain.Services
+{
+    public class TicketService : ITicketService
+    {
+    }
+}
