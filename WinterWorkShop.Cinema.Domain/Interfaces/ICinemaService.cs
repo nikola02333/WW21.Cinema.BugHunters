227091adcbe37fc7d055e7fd3f492eb1202e8dd9
Nemanja Svilarov <nemanja.svilarov@gmail.com>
2021-03-10 10:50:34 +0100

    ICInemaService

--- WinterWorkShop.Cinema.Domain/Interfaces/ICinemaService.cs
+++ WinterWorkShop.Cinema.Domain/Interfaces/ICinemaService.cs
@@ -9,5 +9,11 @@ namespace WinterWorkShop.Cinema.Domain.Interfaces
     public interface ICinemaService
     {
         Task<IEnumerable<CinemaDomainModel>> GetAllAsync();
+        Task<GenericResult<CinemaDomainModel>> AddCinemaAsync(CinemaDomainModel newCinema);
+
+        GenericResult<CinemaDomainModel> DeleteCinema(int id);
+        Task<GenericResult<CinemaDomainModel>> GetCinemaById(int id);
+
+        Task<GenericResult<CinemaDomainModel>> UpdateCinema(CinemaDomainModel updateCinema);
     }
 }
