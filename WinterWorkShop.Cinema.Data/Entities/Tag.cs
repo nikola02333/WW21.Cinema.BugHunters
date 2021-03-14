3c78f29743e53ccd4afe4fc65261d58af7c760e6
Nikola Velickovic <nikola.velickovic@vega.local>
2021-03-14 16:14:37 +0100

    Added TagMovies table

---
+++ WinterWorkShop.Cinema.Data/Entities/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinterWorkShop.Cinema.Data.Entities
+{
+   public class Tag
+    {
+        public string TagId { get; set; }
+
+        public string? TagValue { get; set; }
+
+        public ICollection<TagsMovies> TagsMovies { get; set; }
+    }
+}
