4239beee4be2cbbb0350f2e7f558561165c07e07
Nemanja Svilarov <nemanja.svilarov@gmail.com>
2021-03-16 14:23:08 +0100

    Commited changes for tests

--- WinterWorkShop.Cinema.API.Tests/Controllers/AuditoriumControllerTests.cs
+++ WinterWorkShop.Cinema.API.Tests/Controllers/AuditoriumControllerTests.cs
@@ -1,7 +1,18 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Castle.Core.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using WinterWorkShop.Cinema.API.Controllers;
+using WinterWorkShop.Cinema.API.Models;
+using WinterWorkShop.Cinema.Domain.Common;
+using WinterWorkShop.Cinema.Domain.Interfaces;
+using WinterWorkShop.Cinema.Domain.Models;
 
 namespace WinterWorkShop.Cinema.Tests.Controllers
 {
