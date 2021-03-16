716f67b6f323b7a9fcfe1092d29da100c878bf0a
Nikola Velickovic <nikola.velickovic@vega.local>
2021-03-16 10:29:54 +0100

    JWT Autorization and Authentication completed

--- WinterWorkShop.Cinema.API/Controllers/DemoAuthenticationController.cs
+++ WinterWorkShop.Cinema.API/Controllers/DemoAuthenticationController.cs
@@ -6,7 +6,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WinterWorkShop.Cinema.API.Models;
 using WinterWorkShop.Cinema.API.TokenServiceExtensions;
+using WinterWorkShop.Cinema.Domain.Common;
+using WinterWorkShop.Cinema.Domain.Interfaces;
+using WinterWorkShop.Cinema.Domain.Models;
 
 namespace WinterWorkShop.Cinema.API.Controllers
 {
@@ -14,22 +18,41 @@ namespace WinterWorkShop.Cinema.API.Controllers
     public class DemoAuthenticationController : ControllerBase    
     {
         private readonly IConfiguration _configuration;
-
-        public DemoAuthenticationController(IConfiguration configuration)
+        private readonly IUserService _userService;
+    
+        public DemoAuthenticationController(IConfiguration configuration,IUserService userService)
         {
             _configuration = configuration;
+            _userService = userService;
         }
 
         // NOT FOR PRODUCTION USE!!!
         // you will need a robust auth implementation for production
         // i.e. try IdentityServer4
+        [HttpGet]
         [Route("/get-token")]
-        public IActionResult GenerateToken(string name, bool admin)
+        public async Task<ActionResult> GenerateToken([FromBody]UserLoginModel userLogin)
         {
-            var jwt = JwtTokenGenerator
-                .Generate(name, admin, _configuration["Tokens:Issuer"], _configuration["Tokens:Key"]);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
-            return Ok(new {token = jwt});
+            var userExists = await _userService.GetUserByUserNameAsync(userLogin.UserName);
+
+            
+            if(userExists.IsSuccessful)
+            {
+
+
+                var jwt = JwtTokenGenerator
+                .Generate(userLogin.UserName, userExists.Data.Role, _configuration["Tokens:Issuer"], _configuration["Tokens:Key"]);
+
+                return Ok(new { token = jwt });
+            }
+
+            return Unauthorized();
+            
         }
     }
 }
