da6ca11250c312ac63e3c2787f3dfb1b42d93938
Nemanja Svilarov <nemanja.svilarov@gmail.com>
2021-03-16 14:21:31 +0100

    Commit

---
+++ WinterWorkShop.Cinema.API.Tests/Controllers/AuditoriumControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinterWorkShop.Cinema.Tests.Controllers
+{
+
+    [TestClass]
+    public class AuditoriumControllerTests
+    {
+    }
+}
