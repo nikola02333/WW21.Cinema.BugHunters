01d9577a9f09d64550ae36dcad8acd433c1f4639
Nikola Velickovic <nikola.velickovic@vega.local>
2021-03-26 13:46:41 +0100

    aded imdb API

--- WinterWorkShop.Cinema.API/Controllers/ImdbsController.cs
+++ WinterWorkShop.Cinema.API/Controllers/ImdbsController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IMDbApiLib;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using WinterWorkShop.Cinema.Domain.Common;
 using WinterWorkShop.Cinema.Domain.Models;
@@ -17,6 +19,9 @@ namespace WinterWorkShop.Cinema.API.Controllers
         [Route("TegTopTenMovies")]
         public async Task<ActionResult<GenericResult<MovieDomainModel>>> GetTopTenMovies()
         {
+          
+            var apiLib = new ApiLib("k_9szm9guo");
+            var data = await apiLib.SearchMovieAsync("Inception 2010");
 
             return null;
         }
